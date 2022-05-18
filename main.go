557709eea40c37727592f2cd35bd1af16134ebfb
ev <dmitry_dtv@mail.ru>
2022-05-19 00:31:24 +0500

    220518_3
    redis bd cut

--- main.go
+++ main.go
@@ -57,43 +57,6 @@ var ctx = context.Background()
 func main() {
 	My_redis.Main()
 
-	//redis start
-	// rdb := redis.NewClient(&redis.Options{
-	// 	Addr:     "localhost:6379",
-	// 	Password: "", // no password set
-	// 	DB:       0,  // use default DB
-	// })
-
-	// userid := "user#125"
-	// currentusertoken := "testtoken"
-
-	// node := rdb.Set(ctx, userid, currentusertoken, 0).Err()
-	// if node != nil {
-	// 	panic(node)
-	// }
-
-	// val, node := rdb.Get(ctx, userid).Result()
-	// if node == redis.Nil {
-	// 	fmt.Println("key1 does not exist")
-	// } else if node != nil {
-	// 	panic(node)
-	// } else {
-	// 	fmt.Println(userid, val)
-	// }
-
-	// val2, node := rdb.Get(ctx, "key2").Result()
-	// if node == redis.Nil {
-	// 	fmt.Println("key2 does not exist")
-	// } else if node != nil {
-	// 	panic(node)
-	// } else {
-	// 	fmt.Println("key2", val2)
-	// }
-	// Output: key value
-	// key2 does not exist
-
-	//redis end
-
 	Log = "root111"
 	Pass = "1111"
 
